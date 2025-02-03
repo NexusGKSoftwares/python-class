e69aa064f1faff3f72e842b4c73518b23e56b469
NexusGKSoftwares <nexusgksoftwares@gmail.com>
2025-02-03 21:42:22 +0300

    first commit

---
+++ introduction.py
@@ -0,0 +1,27 @@
+#  introduction to python
+
+# what is python?
+# is a high-level, interpreted programming language 
+# that is widely used for various purposes such as web development, scientific computing, data analysis, ai
+
+# why learn python?
+# simple and easy to learn
+# large community and resources
+#  extensive libraries and frameworks e.g pandas
+#  cross-platform compatibility
+
+
+# ide or text editor?
+#  -vs code
+#  -pycharm
+#  -sublime text
+
+# 2 python syntax, variables and data types
+
+# python syntax
+#  -indentation is used to define block of code
+
+#  -print() function is used to print output
+print("hello world") # prints hello world
+
+
